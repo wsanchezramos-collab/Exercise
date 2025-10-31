91fe122d69f99026937c3825d8824861998aec8f
William Sanchez-Ramos <William@example.com>
2025-10-31 14:48:22 -0400

    Final Project

--- Stats.py
+++ Stats.py
@@ -6,12 +6,13 @@ def prop_above(ls):
     for items in last_list:
         if items > average(ls):
             i+=1
-    return (i/len(ls))*100
+    return (i/len(ls))
 def median(ls):
     middle = int(len(ls)/2)
-    middle_1 = middle +1 
-    return (ls[middle]+ls[middle_1])/2
-
+    middle_1 = ls[middle]
+    return middle_1
+    
+ 
 def make_hist(data,n_bins):
     plt.hist(data,bins = n_bins)
     plt.title("Hours of Exercise")
@@ -29,12 +30,11 @@ for line in fhand:
 for numbers in new_list:
     last_list.append(float(numbers))
 sorting = last_list.sort()
-print("The Average is",int(average(last_list)))
-print("The proprotions above the average are at ",int(prop_above(last_list)),"%")
+print("The Average is",average(last_list))
+print("The proprotions above the average are at ",prop_above(last_list))
 print("The median is",median(last_list))
 make_hist(last_list,15)
 
-    
 
 
     
